420e7701263896edf2c64a10d541de85a844eaee
IbrahimHindawi <ihindawi@gmail.com>
2023-12-04 19:36:22 +0400

    added ArrayT

---
+++ include/core.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <math.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef float f32;
+typedef double f64;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+typedef size_t usize;
+
+#define CAT_( a, b ) a##b
+#define CAT( a, b ) CAT_( a, b )
