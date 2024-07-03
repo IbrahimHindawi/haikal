1b37f54a8bb35d95405fb703859a1c8cfdc66f0e
IbrahimHindawi <ihindawi@gmail.com>
2024-07-03 22:51:17 +0400

    upgraded meta to support templates

--- src/meta.c
+++ src/meta.c
@@ -24,6 +24,26 @@
 
 const char *metapath = NULL;
 
+structdef(Node_bstring) {
+    bstring data;
+    i32 foundat;
+    Node_bstring *next;
+};
+
+Node_bstring *Node_bstring_create(bstring input, i32 foundat) {
+    Node_bstring *result = malloc(sizeof(Node_bstring));
+    if (result == NULL) { exit(-1);}
+    result->data = input;
+    result->next = NULL;
+    result->foundat = foundat;
+    return result;
+}
+
+void Node_bstring_destroy(Node_bstring *node) {
+    bdestroy(node->data);
+    // node->next = 
+}
+
 char *getCurrentWorkingDirectory() {
     const i32 buffersize = 256;
     char *cwdstr = malloc(buffersize);
@@ -53,7 +73,7 @@ void metainit(str metaname) {
     bcatcstr(typecorepath, ".h");
     // printf("typecorepath: %s\n", bdata(typecorepath));
     FILE *output = NULL;
-    if (null != (output = fopen(bdata(typecorepath), "w"))) {
+    if (NULL != (output = fopen(bdata(typecorepath), "w"))) {
         bstring result = bfromcstr("#pragma once\n");
         fputs(bdatae(result, "NULL"), output);
         bdestroy(result);
@@ -69,8 +89,8 @@ void metainit(str metaname) {
  * genpath/metaname.h <---append--- genpath/metaname_genname.h
  */
 int metagen(str metaname, str genname) {
-    FILE *input = null;
-    FILE *output = null;
+    FILE *input = NULL;
+    FILE *output = NULL;
     struct bstrList *lines;
     struct tagbstring postfix = bsStatic("\n");
     bstring stubinclude = bfromcstr("#include \"TYPE.h\"");
@@ -96,17 +116,17 @@ int metagen(str metaname, str genname) {
     bcatcstr(typemetapath, ".h");
     // printf("typemetapath: %s\n", bdata(typemetapath));
 
-    if (null != (input = fopen(bdata(typecorepath), "r"))) {
+    if (NULL != (input = fopen(bdata(typecorepath), "r"))) {
         bstring b = bread((bNread) fread, input);
         fclose(input);
-        if (null != (lines = bsplit(b, '\n'))) {
+        if (NULL != (lines = bsplit(b, '\n'))) {
             for (int i = 0; i < lines->qty; ++i) {
                 bfindreplace(lines->entry[i], stubinclude, emptystr, 0);
                 bfindreplace(lines->entry[i], typestr, typemetastr, 0);
                 binsert(lines->entry[i], blength(lines->entry[i]), &postfix, '?');
                 // printf("%04d: %s\n", i, bdatae(lines->entry[i], "NULL"));
             }
-            if (null != (output = fopen(bdata(typemetapath), "w"))) {
+            if (NULL != (output = fopen(bdata(typemetapath), "w"))) {
                 for (int i = 0; i < lines->qty; ++i) {
                     fputs(bdatae(lines->entry[i], "NULL"), output);
                 }
@@ -125,7 +145,7 @@ int metagen(str metaname, str genname) {
     bcatcstr(typecorepathtarget, metaname);
     bcatcstr(typecorepathtarget, ".h");
     // printf("typecorepathtarget: %s\n", bdata(typecorepathtarget));
-    if (null != (output = fopen(bdata(typecorepathtarget), "a"))) {
+    if (NULL != (output = fopen(bdata(typecorepathtarget), "a"))) {
         bstring result = bfromcstr("#include \"");
         bstring typename = bfromcstr(metaname);
         bconcat(result, typename);
@@ -240,21 +260,113 @@ int main(int argc, char *argv[]) {
 		for (int i = 0; i < l; i++) {
 			int keylen;
 			const char *key = toml_table_key(meta_tbl, i, &keylen);
-			printf("haikal::metainit::key[%d]: %s\n", i, key);
-            metainit(key);
-            metacore(key);
+			// printf("haikal::metainit::key[%d]: %s\n", i, key);
+            // metainit(key);
+            // metacore(key);
 
             toml_array_t *arr = toml_table_array(meta_tbl, key);
             if (arr) {
                 int l = toml_array_len(arr);
                 for (int i = 0; i < l; i++) {
-                    printf("  haikal::metagen::index[%d]: %s\n", i, toml_array_string(arr, i).u.s);
-                    metagen(key, toml_array_string(arr, i).u.s);
+                    // printf("  haikal::metagen::index[%d]: %s\n", i, toml_array_string(arr, i).u.s);
+                    // metagen(key, toml_array_string(arr, i).u.s);
                 }
                 printf("\n");
             }
         }
 	}
+
+    bstring cpath = bfromcstr(cwdstr);
+    bstring cmainpath = bfromcstr("/src/main.c");
+    bconcat(cpath, cmainpath);
+    printf("haikal::main::cpath::%s\n", bdata(cpath));
+    // input = fopen(bdata(cpath), "r");
+    // if (!input) {
+    //     printf("haikal::Failed to open main.c\n");
+	// 	exit(1);
+    // }
+    // fseek(input, 0L, SEEK_END);
+    // file_size = ftell(input);
+    // // printf("haikal::toml::file::size::%llu\n", file_size);
+    // // fseek(input, 0L, SEEK_SET);
+    // rewind(input);
+    // buffer = malloc(file_size);
+    // if (!buffer) {
+    //     printf("failed to allocate memory for input c file.\n");
+    // }
+    // ret = fread(buffer, sizeof(*buffer), file_size, input);
+    // buffer[ret] = '\0';
+    // printf("%s\n", buffer);
+    // // printf("ret = %lu, sizeofarray(buffer) = %ld\n", ret, sizeofarray(buffer));
+    // // if (ret != sizeofarray(buffer)) { fprintf(stderr, "fread() failed: %zu\n", ret); exit(EXIT_FAILURE); }
+    // fclose(input);
+
+    struct bstrList *lines;
+    Node_bstring *head = NULL;
+    bstring hktag = bfromcstr("//haikal@");
+
+    if (NULL != (input = fopen(bdata(cpath), "r"))) {
+        bstring b = bread((bNread) fread, input);
+        fclose(input);
+        if (NULL != (lines = bsplit(b, '\n'))) {
+            for (int i = 0; i < lines->qty; ++i) {
+                // bfindreplace(lines->entry[i], stubinclude, emptystr, 0);
+                // bfindreplace(lines->entry[i], typestr, typemetastr, 0);
+                // binsert(lines->entry[i], blength(lines->entry[i]), &postfix, '?');
+                // printf("%04d: %s\n", i, bdatae(lines->entry[i], "NULL"));
+                int found = binstr(lines->entry[i], 0, hktag);
+                if (found != BSTR_ERR) {
+                    printf("haikal tag detected in main.c: %s\n", bdata(lines->entry[i]));
+                    // printf("%s\n", bdata(iter->data));
+                    if (head == NULL) {
+                        head = Node_bstring_create(lines->entry[i], found);
+                        printf("head initalized with: %s\n", bdata(head->data));
+                    }
+                    Node_bstring *iter = head;
+                    while (iter->next != NULL) {
+                        iter = iter->next;
+                    }
+                    // bug: head is initalized so append happens twice!
+                    iter->next = Node_bstring_create(lines->entry[i], found);
+                }
+            }
+            Node_bstring *iter = head;
+            if (head != NULL) {
+                printf("linkedlist walk: {bstring: '%s', foundat: %d, next: %p}\n", bdata(head->data), head->foundat, head->next);
+                iter = head;
+                while (iter->next != NULL) {
+                    bstring result = bmidstr(iter->data, iter->foundat + hktag->slen, iter->data->slen - (iter->foundat + hktag->slen));
+                    struct bstrList *hkCommand = bsplit(result, ':');
+                    printf("hkCommand[0] = %s\n", bdata(hkCommand->entry[0]));
+                    printf("haikal::metainit::%s\n", bdata(hkCommand->entry[0]));
+                    metainit(bdata(hkCommand->entry[0]));
+                    iter = iter->next;
+                    printf("linkedlist walk: {bstring: '%s', foundat: %d, next: %p}\n", bdata(iter->data), iter->foundat, iter->next);
+                }
+                iter = head;
+                while (iter->next != NULL) {
+                    bstring result = bmidstr(iter->data, iter->foundat + hktag->slen, iter->data->slen - (iter->foundat + hktag->slen));
+                    // printf("result = %s\n", bdata(result));
+                    struct bstrList *hkCommand = bsplit(result, ':');
+                    printf("haikal::metagen::%s\n", bdata(hkCommand->entry[0]));
+                    printf("hkCommand[1] = %s:%s\n", bdata(hkCommand->entry[0]), bdata(hkCommand->entry[1]));
+                    metagen(bdata(hkCommand->entry[0]), bdata(hkCommand->entry[1]));
+                    iter = iter->next;
+                    printf("linkedlist walk: {bstring: '%s', foundat: %d, next: %p}\n", bdata(iter->data), iter->foundat, iter->next);
+                }
+            } else {
+                printf("metagen::main::error::linkedlist is empty!\n");
+            }
+            // DANGER! don't destroy list before using the linked list!
+            bstrListDestroy(lines);
+        } else {
+            printf("metagen::main::error::line read error!\n");
+        }
+        bdestroy(b);
+    } else {
+        printf("metagen::main::error::Unable to open main.c file.\n");
+    }
+
     printf("haikal::CodeGen::Finalize.\n");
     return 0;
 }
