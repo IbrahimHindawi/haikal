3e21026a9f9081c4ee59df149c0ea2a2f811f74f
IbrahimHindawi <ihindawi@gmail.com>
2024-07-12 05:09:48 +0400

    upadted list

--- src/meta/hkList_TYPE.h
+++ src/meta/hkList_TYPE.h
@@ -12,9 +12,15 @@ structdef(hkList_TYPE) {
 };
 
 hkList_TYPE *hkList_TYPE_create();
-void hkList_TYPE_append(hkList_TYPE *list, TYPE data);
+void hkList_TYPE_insert_at(hkList_TYPE *list, i32 index);
+void hkList_TYPE_append(hkList_TYPE *list, TYPE item);
+void hkList_TYPE_prepend(hkList_TYPE *list, TYPE item);
+i32 hkList_TYPE_get_length(hkList_TYPE *list);
+hkNode_TYPE hkList_TYPE_remove(hkList_TYPE *list, TYPE item);
+hkNode_TYPE hkList_TYPE_remove_at(hkList_TYPE *list, i32 index);
 void hkList_TYPE_destroy(hkList_TYPE **list);
 
+#define HKLIST_IMPL 1
 #ifdef HKLIST_IMPL
 
 hkList_TYPE *hkList_TYPE_create() {
@@ -25,22 +31,48 @@ hkList_TYPE *hkList_TYPE_create() {
     return list;
 }
 
-void hkList_TYPE_append(hkList_TYPE *list, TYPE data) {
+void hkList_TYPE_insert_at(hkList_TYPE *list, i32 index) {
+}
+
+void hkList_TYPE_append(hkList_TYPE *list, TYPE item) {
     if (!list->head) {
-        list->head = hkNode_TYPE_create(data);
+        list->head = hkNode_TYPE_create(item);
         list->length += 1;
         return;
     }
     if (!list->tail) {
-        list->tail = hkNode_TYPE_create(data);
+        list->tail = hkNode_TYPE_create(item);
         list->head->next = list->tail;
         list->length += 1;
         return;
     }
-    hkNode_TYPE *temp = hkNode_TYPE_create(data);
+    hkNode_TYPE *temp = hkNode_TYPE_create(item);
     list->tail->next = temp; 
     list->tail = temp;
     list->length += 1;
+}
+
+void hkList_TYPE_prepend(hkList_TYPE *list, TYPE item) {
+    hkNode_TYPE *prependnode = hkNode_TYPE_create(item);
+    list->length += 1;
+    if (!list->head) {
+        list->head = prependnode;
+        return;
+    }
+}
+
+i32 hkList_TYPE_get_length(hkList_TYPE *list) {
+    return list->length;
+}
+
+hkNode_TYPE hkList_TYPE_remove(hkList_TYPE *list, TYPE item) {
+    hkNode_TYPE result = {0};
+    return result;
+}
+
+hkNode_TYPE hkList_TYPE_remove_at(hkList_TYPE *list, i32 index) {
+    hkNode_TYPE result = {0};
+    return result;
 }
 
 void hkList_TYPE_destroy(hkList_TYPE **list) {
