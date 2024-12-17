43138b7d68c664f85d44dabab1a3fae1a5b7db4e
IbrahimHindawi <ihindawi@gmail.com>
2024-12-17 21:53:08 +0400

    hkBiNode macro

--- src/meta/hkBiNode_TYPE.h
+++ src/meta/hkBiNode_TYPE.h
@@ -13,3 +13,12 @@ structdef(hkBiNode_TYPE) {
 hkBiNode_TYPE *hkBiNode_TYPE_create(TYPE data);
 void hkBiNode_TYPE_destroy(hkBiNode_TYPE **node);
 TYPE hkBiNode_TYPE_value(hkBiNode_TYPE *node);
+
+#define hkBiNode_TYPE_get(node, value) \
+    if (!(node)) { \
+        printf(#node \
+                " was null!\n"); \
+        /* debugbreak? */ \
+    } else { \
+        (value) = (node)->data; \
+    }
